014bacb5bf6f68e54bbe377fe20e4cfd1e5f7f18
Jose Antonio Soler Jimenez <jasoljim92@gmail.com>
2018-09-25 18:51:42 +0200

    Cambio de color logrado

--- Joinup/Joinup/ViewModels/NewPlanStep1ViewModel.cs
+++ Joinup/Joinup/ViewModels/NewPlanStep1ViewModel.cs
@@ -13,8 +13,21 @@ namespace Joinup.ViewModels
         #region Attributes
             private Color restaurantFrameColor=ColorUtils.BackgroundColor;
             private Color restaurantTextColor = ColorUtils.PrimaryColor;
+            private string restaurantIconSource = "food.svg";
+        private Dictionary<string, string> replaceMap = new Dictionary<string, string>();
         #endregion
         #region Properties
+        public Dictionary<string, string> ReplaceMap
+        {
+            get
+            {
+                return replaceMap;
+            }
+            set
+            {
+                SetValue( ref replaceMap, value );
+            }
+        }
         public Color RestaurantFrameColor
         {
             get
@@ -37,8 +50,23 @@ namespace Joinup.ViewModels
                 SetValue( ref restaurantTextColor, value );
             }
         }
+        public string RestaurantIconSource
+        {
+            get
+            {
+                return restaurantIconSource;
+            }
+            set
+            {
+                SetValue( ref restaurantIconSource, value );
+            }
+        }
         #endregion
         #region Constructors
+        public NewPlanStep1ViewModel()
+        {
+            RestaurantIconSource = "food.svg";
+        }
         #endregion
         #region Commands
         public ICommand RestaurantCategoryCommand
@@ -55,6 +83,13 @@ namespace Joinup.ViewModels
         {
             RestaurantFrameColor= ColorUtils.PrimaryColor;
             RestaurantTextColor = ColorUtils.BackgroundColor;
+
+
+
+            ReplaceMap = new Dictionary<string, string>()
+                {
+                    { "#FILLCOLOR", "#FFFFFF" }
+                };
         }
         #endregion
     }
