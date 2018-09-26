7489c945622affdb574ce0087c214de3673f8fd6
Jose Antonio Soler Jimenez <jasoljim92@gmail.com>
2018-09-26 18:04:32 +0200

    Creado FontIcon

---
+++ Joinup/Joinup/CustomControls/FontIcon.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Joinup.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Joinup.CustomControls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FontIcon : ContentView
+	{
+        public static readonly BindableProperty IconProperty = BindableProperty.Create(
+                                                 propertyName: "Icon",
+                                                 returnType: typeof( string ),
+                                                 declaringType: typeof( FontIcon ),
+                                                 defaultValue: "",
+                                                 defaultBindingMode: BindingMode.TwoWay,
+                                                 propertyChanged: IconPropertyChanged );
+
+        public string Icon
+        {
+            get { return base.GetValue( IconProperty ).ToString(); }
+            set { base.SetValue( IconProperty, value ); }
+        }
+
+        private static void IconPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (FontIcon) bindable;
+            string iconName = newValue.ToString();
+            IconUtils iconUtils = new IconUtils();
+            control.icon.Text = iconUtils.GetIcon(iconName);
+        }
+
+        public static readonly BindableProperty IconColorProperty = BindableProperty.Create(
+                                         propertyName: "Icon",
+                                         returnType: typeof( Color ),
+                                         declaringType: typeof( FontIcon ),
+                                         defaultValue: Color.Black,
+                                         defaultBindingMode: BindingMode.TwoWay,
+                                         propertyChanged: IconColorPropertyChanged );
+
+        public Color IconColor
+        {
+            get { return (Color)base.GetValue( IconColorProperty ); }
+            set { base.SetValue( IconColorProperty, value ); }
+        }
+
+        private static void IconColorPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (FontIcon) bindable;
+            control.icon.TextColor = (Color)newValue;
+        }
+        public FontIcon ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
