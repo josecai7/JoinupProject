d8ba49e5765115723a587ce205b82a99e536784e
Jose Antonio Soler Jimenez <jasoljim92@outlook.com>
2018-10-02 23:46:28 +0200

    Añadido el searchBar de las direcciones de google

--- Joinup/Joinup/ViewModels/NewPlanStep3ViewModel.cs
+++ Joinup/Joinup/ViewModels/NewPlanStep3ViewModel.cs
@@ -1,7 +1,13 @@
-﻿using Joinup.Common.Models;
+﻿using GalaSoft.MvvmLight.Command;
+using GooglePlaces.Xamarin;
+using Joinup.Common.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace Joinup.ViewModels
 {
@@ -9,8 +15,47 @@ namespace Joinup.ViewModels
     {
         #region Attributes
         private Plan plan;
+        private string addressText;
+        Prediction selectedItem=new Prediction();
+        ObservableCollection<Prediction> predictions = new ObservableCollection<Prediction>();
         #endregion
         #region Properties
+        public string AddressText
+        {
+            get
+            {
+                return addressText;
+            }
+            set
+            {
+                SetValue(ref addressText, value);
+                TextChanged();
+            }
+        }
+        public ObservableCollection<Prediction> Predictions
+        {
+            get
+            {
+                return predictions;
+            }
+            set
+            {
+                SetValue(ref predictions, value);
+            }
+        }
+        public Prediction SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                SetValue(ref selectedItem, value);
+                CogerCoordenadas();
+            }
+        }
+
         #endregion
         #region Constructors
         public NewPlanStep3ViewModel(Plan pPlan)
@@ -19,8 +64,35 @@ namespace Joinup.ViewModels
         }
         #endregion
         #region Commands
+        public ICommand TextChangedCommand
+        {
+            get
+            {
+                return new RelayCommand(TextChanged);
+            }
+        }
+
+        private async void TextChanged()
+        {
+            PlacesAutocomplete places = new PlacesAutocomplete("AIzaSyCFG2-DEKK7EnqzH_tiiKItD_CpaJYGCUg");
+            Predictions predictions = await places.GetAutocomplete(AddressText);
+
+            Predictions = new ObservableCollection<Prediction>(predictions.predictions);
+        }
+        #endregion
+        #region Commands
         #endregion
         #region Methods
+        private async void CogerCoordenadas()
+        {
+            PlacesGeocode placesGeocode = new PlacesGeocode("AIzaSyCFG2-DEKK7EnqzH_tiiKItD_CpaJYGCUg");
+            if (SelectedItem != null)
+            {
+                Geocode geocode = await placesGeocode.GetGeocode(SelectedItem.Description);
+                Debug.WriteLine("Hola" + geocode.results.FirstOrDefault().geometry.location.lat);
+            }
+            
+        }
         #endregion
     }
 }
