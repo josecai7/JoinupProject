cad3d5963e2edf542f83027ef8cde5a61dbe6c2b
Jose Antonio Soler Jimenez <jasoljim92@gmail.com>
2019-01-28 17:42:48 +0100

    Subidos test automaticos

---
+++ Joinup.Tests/ApiRestTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Joinup.Common.Models;
+using Joinup.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Joinup.Tests
+{
+    [TestClass]
+    public class ApiRestTests
+    {
+        public const string url = "https://joinupapi.azurewebsites.net";
+        public const string prefix = "/api";
+        public const string plansController = "/Plans";
+        public const string commentsController = "/Comments";
+        public const string usersController = "/Users/GetUser";
+
+        [TestMethod]
+        public async Task DoLogin()
+        {
+            GetUserRequest getUserRequest = new GetUserRequest();
+            getUserRequest.Email = "jasoljim92@gmail.com";
+
+            var request = JsonConvert.SerializeObject( getUserRequest );
+            var content = new StringContent( request, Encoding.UTF8, "application/json" );
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri( url );
+            var requestUrl = $"{prefix}{usersController}";
+            var response = await client.PostAsync( url, content );
+
+            Assert.IsTrue( response.IsSuccessStatusCode );
+        }
+        [TestMethod]
+        public async Task GetPlansAsync()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri( url );
+
+            var requestUrl = $"{prefix}{plansController}";
+
+            var response = await client.GetAsync( requestUrl );
+            Assert.IsTrue( response.IsSuccessStatusCode );
+        }
+        [TestMethod]
+        public async Task GetCommentsAsync()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri( url );
+
+            var requestUrl = $"{prefix}{commentsController}";
+
+            var response = await client.GetAsync( requestUrl );
+            Assert.IsTrue( response.IsSuccessStatusCode );
+        }
+    }
+}
---
+++ Joinup.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "Joinup.Tests" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Joinup.Tests" )]
+[assembly: AssemblyCopyright( "Copyright ©  2019" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "0dbdd667-1019-41c2-8da6-c1e11d54fe54" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
