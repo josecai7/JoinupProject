3754e46c520902f8e08d0bb4e0d67bde9e5d39e4
Jose Antonio Soler Jimenez <jasoljim92@gmail.com>
2019-02-18 16:18:35 +0100

    typifiers

---
+++ Joinup.Common/Models/FoodType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joinup.Common.Models
+{
+    public class FoodType
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
